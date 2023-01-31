5f88894b3d6134eb8b42a32bdfa06353d37b8765
Gonyshev <maroop@bk.ru>
2023-01-31 18:40:00 +0300

    'second'

--- script.js
+++ script.js
@@ -82,3 +82,78 @@ horsLeft.addEventListener('click', ()=>{
  closeBtn.addEventListener('click', ()=> {
     mobileMenu.style.display = 'none';
  })
+
+ //Свайпер для слайдера
+const publickSlider = document.querySelector('.publick-slider');
+ publickSlider.addEventListener('touchstart', handleTouchStart, false);
+ publickSlider.addEventListener('touchmove', handleTouchMove, false);
+
+ let x1 = null;
+
+ function handleTouchStart(event){
+    
+    const firstTouch = event.touches[0];
+    x1 = firstTouch.clientX;
+ }
+ function handleTouchMove(event){
+    if (!x1){
+        return false;
+    }
+    let x2 = event.touches[0].clientX;
+    let xDiff = x1 - x2
+    if (xDiff < 0){
+        publickPosition -= widthPublick;
+        if (publickPosition < 0) {
+            publickPosition = widthPublick * 2;
+        }
+    }
+    if (xDiff > 0){
+        publickPosition += widthPublick;
+        if (publickPosition > widthPublick * 2) {
+            publickPosition = 0;
+        }
+    }
+
+    publickSlide.forEach((item)=>{ 
+        item.style.left = -publickPosition + 'px';
+        x2 = 0
+        x1 = 0
+    })
+ }
+
+ const horseSlider = document.querySelector('.horse-slider');
+ horseSlider.addEventListener('touchstart', horseTouchStart, false);
+ horseSlider.addEventListener('touchmove', horseTouchMove, false);
+
+ let h1 = null;
+
+ function horseTouchStart(event){
+    
+    const firstTouch = event.touches[0];
+    h1 = firstTouch.clientX;
+ }
+ function horseTouchMove(event){
+    if (!h1){
+        return false;
+    }
+    let x2 = event.touches[0].clientX;
+    let xDiff = h1 - x2
+    if (xDiff < 0){
+        horsePosition -= widthHors + 17;
+        if (horsePosition < 0) {
+            horsePosition = widthHors * 8;
+        }
+    }
+    if (xDiff > 0){
+        horsePosition += widthHors + 17;
+        if (horsePosition > widthHors * 8) {
+            horsePosition = 0;
+        }
+    }
+
+    horsSlide.forEach((item)=>{ 
+        item.style.left = -horsePosition + 'px';
+        x2 = 0
+        h1 = 0
+    })
+ }
